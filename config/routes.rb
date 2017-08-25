6f746da39d48f4ca20a64e61a264b5118f9ce5f5
zannain <zannainshafi@gmail.com>
2017-08-24 20:23:46 -0400

    refactor omniauth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static#index'
+  root to: 'static#index'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
