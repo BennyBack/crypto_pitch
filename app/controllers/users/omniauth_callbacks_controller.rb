718fa607ed696eea734fb9a703664e4bd6453292
zannain <zannainshafi@gmail.com>
2017-08-26 22:31:04 -0400

    reverted to previous commmit because login form completely disappered

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,3 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
  
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   has_many :alerts
   has_many :identities
 
+
 end
