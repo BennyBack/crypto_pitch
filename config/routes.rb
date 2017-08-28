016a6e1b6e4546ea25fa6ee8708cc9d458cb8d84
zannain <zannainshafi@gmail.com>
2017-08-27 23:35:41 -0400

    Set up AJAX on page displaying table of cryptocurrency

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static#index'
+  resources :users do
+    resources :alerts
+  end
+  get 'search', to: 'alerts#index', as: 'search'
   get 'about', to: 'static#about', as: 'about'
   get 'dashboard', to: 'static#dashboard', as: 'dashboard'
-  resources "alerts"
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
