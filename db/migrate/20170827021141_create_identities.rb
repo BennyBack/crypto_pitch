1fe04f54323d5c557947e0ecd3ce689026c7de82
zannain <zannainshafi@gmail.com>
2017-08-30 12:40:54 -0400

    removed flash notice temporarily from index.html

--- db/migrate/20170827021141_create_identities.rb
+++
@@ -1,9 +0,0 @@
-class CreateIdentities < ActiveRecord::Migration[5.1]
-  def change
-    create_table :identities do |t|
-      t.belongs_to :user, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
