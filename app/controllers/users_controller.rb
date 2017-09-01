b33a436a2557c78d35052f3e2a824ca5037c9ba2
Zannain Shafi <zannainshafi@gmail.com>
2017-09-01 15:08:02 -0400

    adding bcrypt

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: :show
+
+    def index 
+    end
+
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+
+        else
+            render 'new'
+        end
+    end
+    
+    
+    def show
+    end
+
+    private
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email, :phone_number, :password, :password_confirmation)
+    end
+
+end
