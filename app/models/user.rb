50c4cc0ac91a2af71d2e95d448a9c8d15ae43f85
Aaron <Aaronmwq@gmail.com>
2017-09-01 20:04:56 -0400

    styling user dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  after_create :create_profile
   validates :first_name, :last_name, :phone_number,:email, :presence => true
+  validates :phone_number :uniqueness => true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
